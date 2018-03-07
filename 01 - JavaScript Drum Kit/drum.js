2f0be6ca086d90bbfa99b5fbd8555f10e0924a95
Roland Wright <roland.wright@korecreative.io>
2018-03-06 22:35:28 -0700

    Added Javascripts
    Added Javascripts

--- 01 - JavaScript Drum Kit/drum.js
+++ 01 - JavaScript Drum Kit/drum.js
@@ -1,13 +1,17 @@
-  window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; // stop the function from running
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
+//   window.addEventListener('keydown', function(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//     if (!audio) return; // stop the function from running
+//     audio.currentTime = 0;
+//     audio.play();
+//     key.classList.add('playing');
+// });
+//   function removeTransition(e) {
+//     console.log(e);
+// }
+//  const keys = document.querySelectorAll('.key');
+//  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
 });
-  function removeTransition(e) {
-    console.log(e);
-}
- const keys = document.querySelectorAll('.key');
- keys.forEach(key => key.addEventListener('transitionend', removeTransition));
