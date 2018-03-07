d93c127cdb65269f1a0a166a97b5b045651d830d
Roland Wright <roland.wright@korecreative.io>
2018-03-06 22:40:04 -0700

    Added Javascripts
    Added Javascripts

--- 01 - JavaScript Drum Kit/drum.js
+++ 01 - JavaScript Drum Kit/drum.js
@@ -14,4 +14,5 @@
 
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
+  const key = document.querySelector('.key[data-key="${e.keyCode}"]');
 });
